4796bc942f7fbde97684456048b75f48a7450a1b
Ray Wofford <raywofforddesign@gmail.com>
2024-04-07 02:37:55 -0500

    moved some code in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,5 @@
 import adapter from '@sveltejs/adapter-netlify';
 
-
-export default {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
-};
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import { mdsvex } from 'mdsvex'
@@ -42,4 +25,19 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+	config
+	kit: {
+		// default options are shown
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	}
+};
